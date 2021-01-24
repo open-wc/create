91c47d29ee04bc009ed862eae6abbf4cdb7b562c
Stephen Wade <stephen@stephenwade.me>
2021-01-24 12:03:50 -0500

    chore: commit snapshot changes

--- test/snapshots/fully-loaded-app/web-test-runner.config.mjs
+++ test/snapshots/fully-loaded-app/web-test-runner.config.mjs
@@ -1,9 +1,29 @@
-import baseConfig from './web-dev-server.config.mjs';
+// import { playwrightLauncher } from '@web/test-runner-playwright';
 
 export default /** @type {import("@web/test-runner").TestRunnerConfig} */ ({
   files: 'test/**/*.test.js',
+  nodeResolve: true,
 
-  /** Reuse options from Web Dev Server config  */
-  nodeResolve: baseConfig.nodeResolve,
-  plugins: [...baseConfig.plugins],
+  /** Compile JS for older browsers. Requires @web/dev-server-esbuild plugin */
+  // esbuildTarget: 'auto',
+
+  /** Confgure bare import resolve plugin */
+  // nodeResolve: {
+  //   exportConditions: ['browser', 'development']
+  // },
+
+  /** Amount of browsers to run concurrently */
+  // concurrentBrowsers: 2,
+
+  /** Amount of test files per browser to test concurrently */
+  // concurrency: 1,
+
+  /** Browsers to run tests on */
+  // browsers: [
+  //   playwrightLauncher({ product: 'chromium' }),
+  //   playwrightLauncher({ product: 'firefox' }),
+  //   playwrightLauncher({ product: 'webkit' }),
+  // ],
+
+  // See documentation for all available options
 });
