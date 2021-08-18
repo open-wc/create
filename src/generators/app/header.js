cdf2e8e764c2cb7b93e5b65d47b4567f2a34ed69
Benny Powers <web@bennypowers.com>
2021-08-18 14:05:24 +0300

    feat: --help command
    Also removes "extending" docs from README (see https://github.com/open-wc/open-wc/pull/2270)

--- src/generators/app/header.js
+++ src/generators/app/header.js
@@ -7,10 +7,10 @@ export default `
  .d'    Web Components  \`b.    Start or upgrade your web component project with
  .d'                     \`b.   ease. All our recommendations at your fingertips.
  ::   .................   ::
- \`p.                     .q'   See more details at https://open-wc.org/init/
+ \`p.                     .q'
   \`p.    open-wc.org    .q'
    \`b.     @openWc     .d'
-     \`q..            ..,'      Note: you can exit any time with Ctrl+C or Esc
+     \`q..            ..,'      See more details at https://open-wc.org/init/
         '',,,,,,,,,,''
 
 `;
