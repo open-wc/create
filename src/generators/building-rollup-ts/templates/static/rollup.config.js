f37eaaf018ce1aae5e24773cf1ac5b0d8e0094ab
Westbrook Johnson <westbrook.johnson@gmail.com>
2024-05-25 17:57:43 -0400

    deps: use latest @web versions

--- src/generators/building-rollup-ts/templates/static/rollup.config.js
+++ src/generators/building-rollup-ts/templates/static/rollup.config.js
@@ -1,6 +1,6 @@
 import nodeResolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
-import html from '@web/rollup-plugin-html';
+import { rollupPluginHTML as html } from '@web/rollup-plugin-html';
 import { importMetaAssets } from '@web/rollup-plugin-import-meta-assets';
 import esbuild from 'rollup-plugin-esbuild';
 import { generateSW } from 'rollup-plugin-workbox';
